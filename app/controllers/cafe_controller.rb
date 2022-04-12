c67749ecaaaacf5724667e0e9e569c7059ae8247
Flynn <flynnwebdev@gmail.com>
2022-04-12 14:16:38 +1000

    Afternoon session

--- app/controllers/cafe_controller.rb
+++ app/controllers/cafe_controller.rb
@@ -1,4 +1,10 @@
 class CafeController < ApplicationController
     def index
+        @menu = [
+            { name: "Latte", price: 4.0 },
+            { name: "Scone", price: 5.0 },
+            { name: "Tea", price: 3.5 }
+        ]
+        # @menu = { latte: 4.0, scone: 5.0, tea: 3.0 }
     end
 end
